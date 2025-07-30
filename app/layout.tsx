cbaf27b4ee058444917820d7db7fc049248a0993
Haikyo <exist02593@gmail.com>
2025-07-30 23:01:51 +0800

    feat: update web favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from "@vercel/analytics/next"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
-
+const baseUrl = process.env.NEXTAUTH_URL
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -14,14 +14,32 @@ export const metadata: Metadata = {
   keywords: [
     '台股法說會', '法說會簡報', '台積電法說會簡報', '2330法說會簡報', '鴻海法說會簡報', '2317法說會簡報',
     '法人說明會', '投資人說明會', '財報說明會', '台灣股市法說會', '上市公司法說會', '上櫃公司法說會',
-    '法說會PDF', '法說會下載', '投資人簡報', '財報簡報', '股票法說會', '公司法說會',
-    '台股投資人說明會', '證券法說會', '股市簡報', '上市簡報', '櫃買簡報', '興櫃簡報'
+    '法說會 PDF', '法說會下載', '投資人簡報', '財報簡報', '股票法說會', '公司法說會',
+    '台股投資人說明會', '證券法說會', '股市簡報', '上市簡報', '櫃買簡報', '興櫃簡報', "法說會行事曆", "法說會時間"
   ],
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-96x96.png', sizes: '96x96', type: 'image/png' },
+    ],
+    shortcut: ['/favicon.ico'],
+  },
   openGraph: {
-    title: '台股法說會簡報搜尋',
+    title: '台股法說會簡報資料庫',
     description: '最完整的台灣股市法說會簡報資料庫',
     type: 'website',
     locale: 'zh_TW',
+    url: baseUrl,
+      images: [
+      {
+        url: `${baseUrl}/FinmoAI-brand.png`,
+        width: 1200,
+        height: 630,
+        alt: "台股法說會簡報資料庫",
+        type: "image/png",
+      },
+    ],
   },
   verification: {
     google: 'eY_A1hOpUgH61VizjCcMHu_JUGPZsleGNzR-SzeiM0Q'
