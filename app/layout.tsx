14a2c3e678388d6127e2ab7dccf79d800277512b
Haikyo <exist02593@gmail.com>
2025-12-22 19:14:53 +0800

    feat: add gtm into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -88,15 +88,14 @@ export default function RootLayout({
   return (
     <html lang="zh-TW" className="antialiased">
       <head>
-        {/* Google tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-4BJJPNVK5C"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-4BJJPNVK5C');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W5JDVDM5');
             `,
           }}
         />
@@ -152,6 +151,15 @@ export default function RootLayout({
         />
       </head>
       <body className={`${inter.className} min-h-screen bg-neutral-50`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W5JDVDM5"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
         <AdSenseScript />
       </body>
